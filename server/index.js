5be3fdaa7a94b1c19ca55ee5fe384b9d04ccd28d
Sorawong L <Sorawong2543@gmail.com>
2021-12-22 21:49:15 +0700

    Connect to MongoDB using Docker
    Note: End Task MongoDB Compass if you want to connect with Docker. If not, this code will connect with MongoDB Atlas not Docker.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log("Hello MERN + Docker!")
+const { MongoClient } = require("mongodb")
+require("dotenv").config()
+
+// Connection URI
+const url = process.env.DB_URI
+const client = new MongoClient(url)
+
+// Database name
+const dbName = process.env.DB_NAME
+
+async function dbConnect() {
+  try {
+    await client.connect()
+    console.log("Connect to MongoDB success.")
+    const db = client.db(dbName)
+    const data = await db.collection("post").findOne({ name: "wong" })
+    console.log(data)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+dbConnect()
